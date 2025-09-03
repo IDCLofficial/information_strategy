4d341c412729ff60b44788d8fca6b05855c7dcc3
Joshua-Ogbonna <devjaykes@gmail.com>
2025-09-03 10:09:47 +0100

    chore: work

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   apps: [{
     script: 'npm start',
+    name: 'information-strategy',
+    instances: 1,
+    exec_mode: 'fork',
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      NEXT_PUBLIC_CONTENTFUL_SPACE_ID: 'g08mvfhb24ji',
+      NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN: 'yZmq2UuZ1_8XDAKpc6zdstd8XtD4vEwTc5aqK-zPU-o',
+      NEXT_PUBLIC_CONTENTFUL_PREVIEW_ACCESS_TOKEN: 'rlJ3RsWYOuYx4jdCjveT_2w5ekUNt4e14JuCHCdRfsw',
+      NEXT_PUBLIC_MINISTRY_ID: '3AVAM45hTT76Kk9Yb5vyUh'
+    }
   }],
 
   deploy: {
@@ -12,7 +23,7 @@ module.exports = {
       repo: 'git@github.com:IDCLofficial/information_strategy.git',
       path: '/home/ubuntu',
       'pre-deploy-local': '',
-      'post-deploy': 'source ~/.nvm/nvm.sh && export NEXT_PUBLIC_CONTENTFUL_SPACE_ID=g08mvfhb24ji && export NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN=yZmq2UuZ1_8XDAKpc6zdstd8XtD4vEwTc5aqK-zPU-o && export NEXT_PUBLIC_CONTENTFUL_PREVIEW_ACCESS_TOKEN=rlJ3RsWYOuYx4jdCjveT_2w5ekUNt4e14JuCHCdRfsw && export NEXT_PUBLIC_MINISTRY_ID=3AVAM45hTT76Kk9Yb5vyUh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'source ~/.nvm/nvm.sh && export NODE_OPTIONS="--max-old-space-size=2048" && export NEXT_PUBLIC_CONTENTFUL_SPACE_ID=g08mvfhb24ji && export NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN=yZmq2UuZ1_8XDAKpc6zdstd8XtD4vEwTc5aqK-zPU-o && export NEXT_PUBLIC_CONTENTFUL_PREVIEW_ACCESS_TOKEN=rlJ3RsWYOuYx4jdCjveT_2w5ekUNt4e14JuCHCdRfsw && export NEXT_PUBLIC_MINISTRY_ID=3AVAM45hTT76Kk9Yb5vyUh && npm ci --production=false && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,36 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["images.ctfassets.net"],
   },
+  // Optimize build performance and memory usage
+  experimental: {
+    // Reduce memory usage during build
+    workerThreads: false,
+    // Optimize bundle size
+    optimizePackageImports: ['react-icons', 'framer-motion'],
+  },
+  // Reduce build memory usage
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Optimize client-side bundle
+      config.optimization.splitChunks = {
+        chunks: 'all',
+        cacheGroups: {
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            priority: -10,
+            chunks: 'all',
+          },
+        },
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
